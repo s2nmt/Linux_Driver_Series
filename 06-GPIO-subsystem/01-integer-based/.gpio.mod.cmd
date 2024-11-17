56abd8e22099d3868124e0fd36fafd8e923cf5eb
s2nmt <tuanzz2k1@gmail.com>
2024-11-17 01:13:36 -0800

    remove build in 06-GPIO-subsystem/01-integer-based

--- 06-GPIO-subsystem/01-integer-based/.gpio.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/s2nmt/work/work_space/06-GPIO-subsystem/01-integer-based/gpio.mod := { echo  /home/s2nmt/work/work_space/06-GPIO-subsystem/01-integer-based/gpio.o;  echo; } > /home/s2nmt/work/work_space/06-GPIO-subsystem/01-integer-based/gpio.mod
