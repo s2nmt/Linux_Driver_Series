efd2aa5b84cad3acf6c3be49d7dcdd00335ac952
s2nmt <tuanzz2k1@gmail.com>
2024-11-22 19:26:57 -0800

    docs: add 04-gpios-direction  05-gpios_group in 06-GPIO-subsystem

---
+++ 06-GPIO-subsystem/05-gpios_group/.gpiog-sysfs.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/s2nmt/work/work_space/06-GPIO-subsystem/05-gpios_group/gpiog-sysfs.mod := { echo  /home/s2nmt/work/work_space/06-GPIO-subsystem/05-gpios_group/gpiog-sysfs.o;  echo; } > /home/s2nmt/work/work_space/06-GPIO-subsystem/05-gpios_group/gpiog-sysfs.mod
---
+++ 06-GPIO-subsystem/05-gpios_group/gpiog-sysfs.mod
@@ -0,0 +1,2 @@
+/home/s2nmt/work/work_space/06-GPIO-subsystem/05-gpios_group/gpiog-sysfs.o
+
